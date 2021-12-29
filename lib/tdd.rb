c25714e6130821237890e8ec9a94c9cab9a4623d
Qien Gao <qiengao@Qiens-MBP.lan>
2021-12-29 14:20:28 -0500

    sec switch

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -3,3 +3,17 @@ def my_uniq(arr)
     arr.each {|ele| count[ele] += 1}
     count.keys
 end
+
+def two_sum(arr)
+    res = []
+    (0...arr.length - 1).each do |i|
+        (i + 1...arr.length).each do |j|
+            res << [i, j] if arr[i] + arr[j] == 0
+        end
+    end
+    res
+end
+
+def my_transpose(arr)
+    (0...arr.length).map { |i| (0...arr.length).map { |j| arr[j][i] } }
+end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -7,6 +7,18 @@ describe '#my_uniq' do
     it 'Takes in an array and returns the unique elements' do
         expect(my_uniq(arr)).to eq([1, 2, 3])
     end
-
-
 end 
+
+describe "#two_sum" do 
+    arr = [-1, 0, 2, -2, 1]
+    it "returns pairs of positions where elements sum to zero" do 
+        expect(two_sum(arr)).to eq([[0, 4], [2, 3]])
+    end
+end
+
+describe "#my_transpose" do
+    arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    it "convert rows to columns" do 
+        expect(my_transpose(arr)).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+    end
+end
